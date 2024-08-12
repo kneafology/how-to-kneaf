43a025b8b8bd20fc798f52a044112867c3a43bcc
andiputraw <andiputraw925@gmail.com>
2024-08-12 17:46:10 +0700

    Feat: Added kneaf.js

---
+++ kneaf/kneaf.js
@@ -0,0 +1,46 @@
+/*
+  Kneaf.js - v1.0
+
+  This module is compatible in the browser and any javascript runtime that has support for "Async-Await" andn "setTimeout()" such as node.js/deno/bun
+
+  To run this module, one can copy all the code to the browser's console, include this to "<script>" tag, or run it with your runtime of choice
+
+  Examples
+  ```html
+  <body>
+    <script src="path/to/kneaf.js">
+  <body>
+  ```
+
+  ```sh
+  node path/to/kneaf.js
+  ```
+
+  Contributors:
+  github:andiputraw -- Created kneaf.js
+
+*/
+
+
+/**
+ * Wait for given amount of time
+ * @param {number} sec. number in seconds
+ * @returns {Promise<void>} Promise indicating completion of waiting
+ */
+function wait(sec) {
+  if(typeof sec !== 'number') throw new Error('sec is not a number')
+  return new Promise((res) => {
+    setTimeout(() => {
+      res()
+    }, sec * 1_000);
+  })
+}
+
+async function main(){
+  while(true){
+    console.log('Kneaf')
+    await wait(5)
+  }
+}
+
+main()
